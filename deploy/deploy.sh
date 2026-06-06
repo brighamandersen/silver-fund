4b7044efb0a6dc742261c416bf050fa4513011fb
Brigham Andersen <brighambandersen@gmail.com>
2026-06-05 23:55:01 -0600

    deploy ~/dev changes

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,12 +5,12 @@ echo "Deploying silverfund"
 
 # frontend
 
-cd /home/brig/code/silver-fund/
+cd /home/brig/dev/silver-fund/
 npm run build
 
 # nginx
 
-sudo cp /home/brig/code/silver-fund/deploy/nginx.conf /etc/nginx/conf.d/silverfund.conf
+sudo cp /home/brig/dev/silver-fund/deploy/nginx.conf /etc/nginx/conf.d/silverfund.conf
 
 sudo nginx -t
 sudo systemctl reload nginx
